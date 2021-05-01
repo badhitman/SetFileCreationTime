0ce7e5a588024dcc7492a662b35fde8e5da8e4cd
badhitman <10000335+badhitman@users.noreply.github.com>
2021-05-01 21:32:48 +0300

    ~ fix
    init

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace SetFileCreationTime
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random gen = new Random();
+            Console.WriteLine("Hello World!");
+            DateTime start = new DateTime(1995, 1, 1);
+            int range = (DateTime.Today - start).Days;
+            gen.Next(range);
+            string current_exe = System.Reflection.Assembly.GetEntryAssembly().Location.ToLower();
+            foreach (string file in Directory.GetFiles(Environment.CurrentDirectory, "*.*", SearchOption.AllDirectories).Where(x => x.ToLower() != current_exe))
+            {
+                DateTime creationTime = File.GetCreationTime(file);
+                Console.WriteLine(creationTime.ToString("MM/dd/yyyy HH:mm:ss"));
+                gen.Next(range);
+                try
+                {
+                    File.SetCreationTime(file, start.AddDays(gen.Next(365, range)).AddHours(gen.Next(1, 20)).AddMinutes(gen.Next(60, 500)).AddSeconds(gen.Next(60, 500)));
+                    Console.WriteLine(File.GetCreationTime(file));
+                }
+                catch
+                {
+
+                }
+            }
+        }
+    }
+}
