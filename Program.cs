3e2ee7f7c09845135d90cfd4607ea4905ed6f22d
badhitman <10000335+badhitman@users.noreply.github.com>
2021-05-01 21:54:14 +0300

    ~ fix

--- Program.cs
+++ Program.cs
@@ -6,22 +6,28 @@ namespace SetFileCreationTime
 {
     class Program
     {
+        static Random gen = new Random();
+        static readonly DateTime start = new DateTime(2005, 1, 1);
+        static readonly int range = (DateTime.Today - start).Days;
+        static string CurrentExeLocation => System.Reflection.Assembly.GetEntryAssembly().Location.ToLower();
+
         static void Main(string[] args)
         {
-            Random gen = new Random();
             Console.WriteLine("Hello World!");
-            DateTime start = new DateTime(1995, 1, 1);
-            int range = (DateTime.Today - start).Days;
+
             gen.Next(range);
-            string current_exe = System.Reflection.Assembly.GetEntryAssembly().Location.ToLower();
-            foreach (string file in Directory.GetFiles(Environment.CurrentDirectory, "*.*", SearchOption.AllDirectories).Where(x => x.ToLower() != current_exe))
+
+            foreach (string file in Directory.GetFiles(Environment.CurrentDirectory, "*.*", SearchOption.AllDirectories).Where(x => x.ToLower() != CurrentExeLocation))
             {
                 DateTime creationTime = File.GetCreationTime(file);
                 Console.WriteLine(creationTime.ToString("MM/dd/yyyy HH:mm:ss"));
-                gen.Next(range);
+                (DateTime, DateTime, DateTime) values = OrganicRndDateTime;
                 try
                 {
-                    File.SetCreationTime(file, start.AddDays(gen.Next(365, range)).AddHours(gen.Next(1, 20)).AddMinutes(gen.Next(60, 500)).AddSeconds(gen.Next(60, 500)));
+                    File.SetCreationTime(file, values.Item1);
+                    File.SetLastWriteTime(file, values.Item2);
+                    File.SetLastAccessTime(file, values.Item3);
+
                     Console.WriteLine(File.GetCreationTime(file));
                 }
                 catch
@@ -30,5 +36,18 @@ namespace SetFileCreationTime
                 }
             }
         }
+
+        static (DateTime, DateTime, DateTime) OrganicRndDateTime
+        {
+            get
+            {
+                DateTime c_rnd_dt = RndDateTime;
+                DateTime w_rnd_dt = c_rnd_dt.AddDays(gen.Next(5, 25));
+                DateTime r_rnd_dt = w_rnd_dt.AddDays(gen.Next(5, 25));
+                return (c_rnd_dt, w_rnd_dt, r_rnd_dt);
+            }
+        }
+
+        static DateTime RndDateTime => start.AddDays(gen.Next(365, range)).AddHours(gen.Next(1, 20)).AddMinutes(gen.Next(60, 500)).AddSeconds(gen.Next(60, 500));
     }
 }
